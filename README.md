fb72e9c2b725e3d4e27ad23d2de4d3fa2b196689
Remco Ykema <r.ykema@tools4ever.com>
2023-03-10 13:24:38 +0100

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
+
 # HelloID-Task-SA-Target-AzureActiveDirectory-AccountGrantGroupMembership
+
+## Prerequisites
+
+## Description
+
+This code snippet executes the following tasks:
+
+1. Define a hash table `$formObject`. The keys of the hash table represent the parameters needed for this action, while the values represent the values entered in the form.
+
+> To view an example of the form output, please refer to the JSON code pasted below.
+
+```json
+{
+    "UserPrincipalName": "testuser@mydomain.local",
+    "GroupsToAdd": [
+        {
+            "name": "testgroup1",
+            "Id" : "599bba95-e5ac-45f9-a3a0-e6e2674bb7df"
+        },
+        {
+            "name": "testgroup2",
+            "Id" : "938a3e5d-2093-4ed9-b6b9-777c144ad08d"
+        }
+    ]
+}
+
+```
+
+> :exclamation: It is important to note that the names of your form fields might differ. Ensure that the `$formObject` hashtable is appropriately adjusted to match your form fields.
---
+++ action.ps1
@@ -0,0 +1,123 @@
+# HelloID-Task-SA-Target-AzureActiveDirectory-AccountGrantGroupMembership
+#########################################################################
+# Form mapping
+$formObject = @{
+    userPrincipalName = $form.UserPrincipalName
+    groupsToAdd       = $form.GroupsToAdd
+}
+
+try {
+    Write-Information "Executing AzureActiveDirectory action: [GrantMembership] for: [$($formObject.userPrincipalName)]"
+
+    # Action logic here
+    Write-Information "Retrieving Microsoft Graph AccessToken for tenant: [$AADTenantID]"
+    $splatTokenParams = @{
+        Uri         = "https://login.microsoftonline.com/$AADTenantID/oauth2/token"
+        ContentType = 'application/x-www-form-urlencoded'
+        Method      = 'POST'
+        Verbose     = $false
+        Body        = @{
+            grant_type    = 'client_credentials'
+            client_id     = $AADAppID
+            client_secret = $AADAppSecret
+            resource      = 'https://graph.microsoft.com'
+        }
+    }
+
+    $accessToken = (Invoke-RestMethod @splatTokenParams).access_token
+    $splatCreateUserParams = @{
+        Uri     = "https://graph.microsoft.com/v1.0/users/$($formObject.userPrincipalName)"
+        Method  = 'GET'
+        Verbose = $false
+        Headers = @{
+            Authorization  = "Bearer $accessToken"
+            Accept         = 'application/json'
+            'Content-Type' = 'application/json'
+        }
+    }
+    $azureADUser = Invoke-RestMethod @splatCreateUserParams
+}
+catch {
+    Write-Error "Could not execute AzureActiveDirectory action [GrantMembership] for: [$($formObject.userPrincipalName)]. User not found in the directory. Error: [$($_.Exception.Message)], Details : [$($_.Exception.ErrorDetails)]"
+    return
+}
+try {
+
+    foreach ( $group in $formObject.GroupsToAdd) {
+
+        try {
+
+            $splatAddParams = @{
+                Uri     = "https://graph.microsoft.com/v1.0/groups/$($group.id)/members/`$ref"
+                Method  = 'POST'
+                Body    = @{ '@odata.id' = "https://graph.microsoft.com/v1.0/users/$($azureADUser.id)" } | ConvertTo-Json -Depth 10
+                Verbose = $false
+                Headers = @{
+                    Authorization  = "Bearer $accessToken"
+                    Accept         = 'application/json'
+                    'Content-Type' = 'application/json'
+                }
+            }
+
+            $null = Invoke-RestMethod @splatAddParams
+            $message = "AzureActiveDirectory action: [GrantMembership to group [$($group.Name)($($group.id))] ] for: [$($formObject.userPrincipalName)] executed successfully"
+            $auditLog = @{
+                Action            = 'GrantMembership'
+                System            = 'AzureActiveDirectory'
+                TargetIdentifier  = "GroupId [$($group.id)]"
+                TargetDisplayName = "User UPN [$formObject.userPrincipalName]"
+                Message           = $message
+                IsError           = $false
+            }
+            Write-Information -Tags 'Audit' -MessageData $auditLog
+            Write-Information $message
+        }
+        catch {
+            $ex = $_
+            if (($ex.Exception.Response) -and ($Ex.Exception.Response.StatusCode -eq 400))  {
+                # 400 indicates already member
+                $message = "AzureActiveDirectory action: [GrantMembership to group [$($group.Name)($($group.id))] ] for: [$($formObject.userPrincipalName)] executed successfully"
+                $auditLog = @{
+                    Action            = 'GrantMembership'
+                    System            = 'AzureActiveDirectory'
+                    TargetIdentifier  = "GroupId [$($group.id)]"
+                    TargetDisplayName = "User UPN [$formObject.userPrincipalName]"
+                    Message           = $message
+                    IsError           = $false
+                }
+                Write-Information -Tags 'Audit' -MessageData $auditLog
+                Write-Information $message
+
+            }
+            else {
+                $message = "Could not execute AzureActiveDirectory action:[GrantMembership to group [$($group.Name)($($group.id))] ] for: [$($formObject.userPrincipalName)], error: $($ex.Exception.Message), Details : [$($ex.ErrorDetails.message)]"
+                $auditLog = @{
+                    Action            = 'GrantMembership'
+                    System            = 'AzureActiveDirectory'
+                    TargetIdentifier  = "GroupId [$($group.id)]"
+                    TargetDisplayName = $formObject.userPrincipalName
+                    Message           = $message
+                    IsError           = $true
+                }
+                Write-Information -Tags "Audit" -MessageData $auditLog
+                Write-Error $message
+            }
+
+        }
+    }
+}
+catch {
+    $ex = $_
+    $message = "Could not execute AzureActiveDirectory action: [GrantMembership] for: [$($formObject.userPrincipalName)], error: $($ex.Exception.Message) details : [$($ex.ErrorDetails.message)] "
+    $auditLog = @{
+        Action            = 'GrantMembership'
+        System            = 'AzureActiveDirectory'
+        TargetIdentifier  = ''
+        TargetDisplayName = $formObject.userPrincipalName
+        Message           = $message
+        IsError           = $true
+    }
+    Write-Information -Tags "Audit" -MessageData $auditLog
+    Write-Error  $message
+}
+#########################################################################
